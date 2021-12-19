06999214e55f669f8caa7bb6b2513c003718aa5d
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-18 21:13:47 -0600

    basicos js

--- curso dalto/capitulo 1/app.js
+++ curso dalto/capitulo 1/app.js
@@ -1,10 +1,10 @@
-// //array
+//array
 
 // players = ["omar", "diego", "beto", "german", "enzo", "claguas", "nacho"];
 
 // console.log(players);
 
-// //while loop.
+//while loop.
 
 // let = number = 1;
 
@@ -13,17 +13,17 @@
 //   number++;
 // }
 
-// //function.
+//function.
 // function greeting(codeName) {
 //   console.log(`hello ${codeName}`);
 // }
 
-// //se reutiliza el codigo, solo se cambia el parametro codeName
+//se reutiliza el codigo, solo se cambia el parametro codeName
 // greeting("pavo");
 // greeting("armando");
 // greeting("pierre");
 
-// //function 2.
+//function 2.
 
 // function sumar(numero1, numero2) {
 //   console.log(numero1 + numero2);
@@ -36,16 +36,16 @@
 // }
 // subtract(150, 100);
 
-// // alert.
-// // alert("hello surfer");
+// alert.
+// alert("hello surfer");
 
-// //prompt
+//prompt
 
-// // let sport = prompt("what sport do you practice??");
+// let sport = prompt("what sport do you practice??");
 
-// // prompt(`so you practice ${sport} every week?`);
+// prompt(`so you practice ${sport} every week?`);
 
-// // if.
+// if.
 
 // let company = "EPAM";
 
@@ -55,7 +55,7 @@
 //   console.log("you wouldnt work with chimo");
 // }
 
-// //operadores
+//operadores
 // let numero = 5;
 
 // numero += 5; // += nos indica que sume 5 al valor del numero
@@ -65,23 +65,23 @@
 // console.log(numero);
 
 // numero **= 2; /* esto significa hacerlo exponencial, si es **=2 es elevarlo al cuadrado,
-//  si es **=3 esto significa elevarlo al cubo y asi sucesivamente */
+//  si es **=3 esto significa elevarlo al cubo y asi sucesivamente
 // console.log(numero);
 
-// // concatenacion
+// concatenacion
 // name = "hello my name is ";
 // real = "omar";
 
 // result = `${name}  ${real} and im practice`;
 // console.log(result);
 
-// //operadores !=
+//operadores !=
 // let texto1 = "numero";
 // let texto2 = "numero";
 
 // console.log(texto1 != texto2);
 
-// //operadores && ||
+//operadores && ||
 
 // let valor = 4;
 // let valor2 = 5;
@@ -89,9 +89,9 @@
 // let resultado = valor && valor2;
 // console.log(resultado == true);
 
-// //condicionales.
+//condicionales.
 
-// // if
+// if
 
 // let = contraseña = prompt("escribe la contraseña");
 
