8e6d42f6fba799b54109bf45d1ce9119ce917468
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-29 22:17:20 -0600

    getElementById, querySelector, setAttribute, getAttribute

--- curso dalto/capitulo 6/codigo.js
+++ curso dalto/capitulo 6/codigo.js
@@ -1,5 +1,23 @@
-parrafo = document.getElementById("parrafo");
-document.write(parrafo);
+// //getelementbyid
+// parrafo = document.getElementById("parrafo");
+// document.write(parrafo);
 
-media = document.querySelectorAll(".socialMedia");
-document.write(media);
+// //querySelector() devuelve el primer elemento que coincida con el selector indicado
+// media = document.querySelector(".socialMedia");
+// document.write(media);
+
+// //querySelectorAll(), devuelve todos los elementos que coincidan con el selector
+// media = document.querySelectorAll(".socialMedia");
+// document.write(media);
+
+const rango = document.querySelector(".rango");
+
+/*setAttribute() nos permite cambiar el valor de un atributo 
+se le debe indicar el type y luego al type al que quiere cambiar
+como aqui abajo lo cambia al type "text" */
+rango.setAttribute("type", "number");
+
+/* getAttribute() nos permite obtener el valor del atributo, en este caso nos extrae
+el valor del type que seria "number" y dentro de los parentesis le indicamos lo que queremos extraer */
+let valorDelAtributo = rango.getAttribute("type");
+document.write(valorDelAtributo);
